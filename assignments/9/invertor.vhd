e328458e037029d11e26c96c9867d22f4ad6ffc5
Ranko Mirosav <ranko.mirosav@student.etf.unibl.org>
2024-10-19 17:47:50 +0200

    Test issue Ranko #9 : Add test file
    -Created folder 9 and add VHDL file invertor

---
+++ assignments/9/invertor.vhd
@@ -0,0 +1,15 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity invertor is
+	port
+	(
+		input	: in  std_logic;
+		output	: out std_logic
+	);
+end invertor;
+
+architecture in_arch of invertor is
+begin
+	
+end in_arch;
