f6a77c236daef554bdc88a8c5013558e67a9f641
Danilo Cica <danilo.cica@student.etf.unibl.com>
2024-10-25 15:11:14 +0200

    Issue #18 : Test Issue Danilo
    Adding Test file

---
+++ assignments/18/test.vhd
@@ -0,0 +1,14 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity test is
+	port
+	(
+		test_input	: in  std_logic;
+		test_output	: out std_logic
+	);
+end test;
+
+architecture test_arch of test is
+begin
+end test_arch;
