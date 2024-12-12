3b37ed590a59624c14fa11a5d140e8fdb443b92f
Radislav Kosijer <radislavkosijer@gmail.com>
2024-12-12 18:10:55 +0100

    Issue #55: Create modified memory controller (second method) - Modified VHDL file for memory controller Modified testbech file

--- assignments/55/mem_ctrl.vhd
+++ assignments/55/mem_ctrl.vhd
@@ -24,32 +24,32 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
--- ! @brief Entity for the memory controller with back-to-back operation enabled.
+--! @brief Entity for the memory controller with back-to-back operation enabled.
 entity mem_ctrl is
   port (
-    clk_i   : in  std_logic;  -- ! Clock signal
-    rst_i   : in  std_logic;  -- ! Reset signal (active high)
-    mem_i   : in  std_logic;  -- ! Memory access request signal
-    rw_i    : in  std_logic;  -- ! Read/Write control signal
-    burst_i : in  std_logic;  -- ! Burst mode enable signal
-    oe_o    : out std_logic;  -- ! Output enable signal
-    we_o    : out std_logic;  -- ! Write enable signal
-    we_me_o : out std_logic   -- ! Mealy-based write enable signal
+    clk_i   : in  std_logic;  --! Clock signal
+    rst_i   : in  std_logic;  --! Reset signal (active high)
+    mem_i   : in  std_logic;  --! Memory access request signal
+    rw_i    : in  std_logic;  --! Read/Write control signal
+    burst_i : in  std_logic;  --! Burst mode enable signal
+    oe_o    : out std_logic;  --! Output enable signal
+    we_o    : out std_logic;  --! Write enable signal
+    we_me_o : out std_logic   --! Mealy-based write enable signal
   );
 end mem_ctrl;
 
--- ! @brief Architecture implementing the memory controller logic.
+--! @brief Architecture implementing the memory controller logic.
 architecture arch of mem_ctrl is
 
-  -- ! @brief State machine type representing the memory controller states.
-  type t_mc_sm_type is (idle, read1, read2, read3, read4, write);
+  --! @brief State machine type representing the memory controller states.
+  type t_state_type is (idle, read1, read2, read3, read4, write);
 
-  -- ! @brief Registers for current and next states of the state machine.
-  signal state_reg, state_next : t_mc_sm_type;
+  --! @brief Registers for current and next states of the state machine.
+  signal state_reg, state_next : t_state_type;
 
 begin
 
-  -- ! @brief State register process for updating the current state on clock edge.
+  --! @brief State register process for updating the current state on clock edge.
   process(clk_i, rst_i)
   begin
     if rst_i = '1' then
@@ -59,17 +59,18 @@ begin
     end if;
   end process;
 
-  -- ! @brief Next-state logic process implementing state transitions.
+  --! @brief Next-state logic process implementing state transitions.
   process(state_reg, mem_i, rw_i, burst_i)
   begin
+    state_next <= idle; --! Default next state
     case state_reg is
       when idle =>
-        if mem_i = '1' and rw_i = '1' then
-          state_next <= read1;
-        elsif mem_i = '1' and rw_i = '0' then
-          state_next <= write;
-        else
-          state_next <= idle;
+        if mem_i = '1' then
+          if rw_i = '1' then
+            state_next <= read1;
+          else
+            state_next <= write;
+          end if;
         end if;
       when write =>
         if mem_i = '1' then
@@ -86,69 +87,62 @@ begin
           state_next <= read2;
         elsif mem_i = '1' and rw_i = '0' then
           state_next <= write;
-        elsif mem_i = '1' and rw_i = '1' then
-          state_next <= read1;
         else
           state_next <= idle;
         end if;
       when read2 =>
-        if burst_i = '1' then
-          state_next <= read3;
-        else
-          state_next <= read4;
-        end if;
+        state_next <= read3;
       when read3 =>
-        if burst_i = '1' then
-          state_next <= read4;
-        else
-          state_next <= read1;
-        end if;
+        state_next <= read4;
       when read4 =>
-        if mem_i = '1' then
-          if rw_i = '1' then
-            state_next <= read1;
-          else
-            state_next <= write;
-          end if;
+        if mem_i = '1' and rw_i = '1' then
+          state_next <= read1;
+        elsif mem_i = '1' and rw_i = '0' then
+          state_next <= write;
         else
           state_next <= idle;
         end if;
+      when others =>
+        state_next <= idle;
     end case;
   end process;
 
-  -- ! @brief Moore output logic process generating outputs based on the current state.
+  --! @brief Moore output logic process generating outputs based on the current state.
   process(state_reg)
   begin
     -- Default values
     oe_o <= '0';
     we_o <= '0';
-
     case state_reg is
-      when idle =>
-        -- Outputs remain inactive
-        oe_o <= '0';
-        we_o <= '0';
       when write =>
         we_o <= '1';
       when read1 | read2 | read3 | read4 =>
         oe_o <= '1';
+      when others =>
+        oe_o <= '0';
+        we_o <= '0';
     end case;
   end process;
 
-  -- ! @brief Mealy output logic process generating `we_me_o` based on inputs and state.
-  process(state_reg, mem_i, rw_i)
+  --! @brief Mealy output logic process generating `we_me_o` based on inputs and state.
+  process(state_reg, mem_i, rw_i, rst_i)
   begin
-    we_me_o <= '0';  -- ! Default value
-    case state_reg is
-      when idle =>
-        if (mem_i = '1') and (rw_i = '0') then
+    we_me_o <= '0';  --! Default value
+    if rst_i = '1' then
+      we_me_o <= '0';
+    else
+      case state_reg is
+        when idle =>
+          if (mem_i = '1') and (rw_i = '0') then
+            we_me_o <= '1';
+          end if;
+        when write =>
           we_me_o <= '1';
-        end if;
-      when write =>
-      when read1 | read2 | read3 | read4 =>
-        -- No write enable in read states
-        we_me_o <= '0';
-    end case;
+        when read1 | read2 | read3 | read4 =>
+          -- No write enable in read states
+          we_me_o <= '0';
+      end case;
+    end if;
   end process;
 
 end arch;
--- assignments/55/mem_ctrl_tb.vhd
+++ assignments/55/mem_ctrl_tb.vhd
@@ -26,47 +26,67 @@ use ieee.numeric_std.all;
 use ieee.std_logic_textio.all;
 use std.textio.all;
 
--- ! @brief Testbench entity for the memory controller.
+--! @brief Testbench entity for the memory controller.
 entity mem_ctrl_tb is
 end mem_ctrl_tb;
 
--- ! @brief Testbench architecture for verifying the memory controller.
+--! @brief Testbench architecture for verifying the memory controller.
 architecture arch of mem_ctrl_tb is
 
-  -- ! @brief Component declaration for memory controller (DUT).
+  --! @brief Component declaration for memory controller (DUT).
   component mem_ctrl
     port(
-      clk_i   : in  std_logic;   -- ! Clock input
-      rst_i   : in  std_logic;   -- ! Reset input
-      mem_i   : in  std_logic;   -- ! Memory input
-      rw_i    : in  std_logic;   -- ! Read/Write control signal
-      burst_i : in  std_logic;   -- ! Burst mode control signal
-      oe_o    : out std_logic;   -- ! Output enable signal
-      we_o    : out std_logic;   -- ! Write enable signal
-      we_me_o : out std_logic    -- ! Memory write enable signal
+      clk_i   : in  std_logic;   --! Clock input
+      rst_i   : in  std_logic;   --! Reset input
+      mem_i   : in  std_logic;   --! Memory input
+      rw_i    : in  std_logic;   --! Read/Write control signal
+      burst_i : in  std_logic;   --! Burst mode control signal
+      oe_o    : out std_logic;   --! Output enable signal
+      we_o    : out std_logic;   --! Write enable signal
+      we_me_o : out std_logic    --! Memory write enable signal
     );
   end component;
 
-  -- ! @brief Clock period constant for simulation.
+  --! @brief Clock period constant for simulation.
   constant c_CLOCK : time := 20 ns;
 
-  -- ! @brief Number of clock cycles for the simulation.
-  constant c_NUM_OF_CLOCKS : integer := 500;
-
-  -- ! @brief Simulation loop counter.
+  --! @brief Simulation loop counter.
   signal i : integer := 0;
 
-  -- ! @brief Input and output signals for the DUT.
-  signal clk_i_test, rst_i_test, mem_i_test, rw_i_test, burst_i_test : std_logic;
-  signal oe_o_test, we_o_test, we_me_o_test                          : std_logic;
+  --! @brief Input and output signals for the DUT.
+  signal clk_i_test   : std_logic := '0';
+  signal rst_i_test   : std_logic := '0';
+  signal mem_i_test   : std_logic := '0';
+  signal rw_i_test    : std_logic := '0';
+  signal burst_i_test : std_logic := '0';
+  signal oe_o_test    : std_logic;
+  signal we_o_test    : std_logic;
+  signal we_me_o_test : std_logic;
 
-  -- ! @brief File handles for input and output data files.
-  file input_buf  : text;  -- ! Input data file
-  file output_buf : text;  -- ! Output data file
-
+  --! @brief File handles for output data files.
+  file output_buf : text;  --! Output data file
+  type test_vector is record
+    mem_i   :  std_logic;
+    rw_i    :  std_logic;
+    burst_i :  std_logic;
+    oe_o    :  std_logic;
+    we_o    :  std_logic;
+    we_me_o :  std_logic;
+  end record;
+  type test_vector_array is array (natural range <>) of test_vector;
+  -- Test vectors
+  constant test_vectors : test_vector_array := (
+      ('0', 'X', 'X', '0', '0', '0'), -- Reset
+      ('1', '1', '0', '1', '0', '0'), -- Idle -> Read1
+      ('0', '0', '0', '0', '0', '0'), -- Read1 -> Idle
+      ('1', '1', '1', '1', '0', '0'), -- Read1 -> Read2 (burst)
+      ('1', '0', '0', '0', '1', '1'), -- Write state
+      ('1', '1', '1', '1', '0', '0'), -- Burst Read progression
+      ('0', '0', '0', '0', '0', '0')  -- Return to Idle
+  );
 begin
 
-  -- ! @brief Instantiate the Device Under Test (DUT).
+  --! @brief Instantiate the Device Under Test (DUT).
   uut : mem_ctrl
     port map(
       clk_i   => clk_i_test,
@@ -78,105 +98,40 @@ begin
       we_o    => we_o_test,
       we_me_o => we_me_o_test
     );
-  -- ! @brief Reset signal initialization.
-  rst_i_test <= '0';
-  -- ! @brief Generate clock signal for the DUT.
+--  --! @brief Reset signal initialization.
+--  rst_i_test <= '0';
+  --! @brief Generate clock signal for the DUT.
   clk_process : process
   begin
     clk_i_test <= '0';
     wait for c_CLOCK / 2;
     clk_i_test <= '1';
     wait for c_CLOCK / 2;
-    if i = c_NUM_OF_CLOCKS then
-      file_close(output_buf);
-      wait;
-    else
-      i <= i + 1;
-    end if;
   end process clk_process;
 
-  -- ! @brief File-based stimulus generator and output verification process.
+  --! @brief File-based stimulus generator and output verification process.
   testbench : process
-    variable read_col_from_input_buf : line;   -- ! Line read from input file
-    variable write_col_to_output_buf : line;   -- ! Line written to output file
-    variable val_mem_i : std_logic;            -- ! Input: Memory control signal
-    variable val_rw_i  : std_logic;            -- ! Input: Read/Write control
-    variable val_burst_i : std_logic;          -- ! Input: Burst control
-    variable val_oe_o : std_logic;             -- ! Expected Output Enable
-    variable val_we_o : std_logic;             -- ! Expected Write Enable
-    variable val_we_me_o : std_logic;          -- ! Expected Memory Write Enable
-    variable val_comma : character;            -- ! Comma delimiter
-    variable good_num : boolean;               -- ! Validity flag for parsed data
-
   begin
-    -- ! Open input and output files.
-    file_open(input_buf, "..\..\data_files\mem_ctrl_input.csv", read_mode);
-    file_open(output_buf, "..\..\data_files\mem_ctrl_output.csv", write_mode);
+    rst_i_test <= '1';
+    wait for c_CLOCK;
+    rst_i_test <= '0';
 
-    -- ! Write header to the output file.
-    write(write_col_to_output_buf, string'
-    ("#mem_i_test,rw_i_test,burst_i_test,oe_actual,we_actual,we_me_actual,oe_o_test,we_o_test,we_me_o_test,result"));
-    writeline(output_buf, write_col_to_output_buf);
+    for i in 0 to test_vectors'length-1 loop
+      mem_i_test <= test_vectors(i).mem_i;
+      rw_i_test <= test_vectors(i).rw_i;
+      burst_i_test <= test_vectors(i).burst_i;
+      wait until rising_edge(clk_i_test);
+      wait for c_CLOCK/2;
+      report "Test " & integer'image(i+1) & " passed. Outputs: " &  "oe=" & std_logic'image(oe_o_test) &
+           ", we=" & std_logic'image(we_o_test) &  ", we_me=" & std_logic'image(we_me_o_test);
 
-    -- ! Loop through input file lines.
-    while not endfile(input_buf) loop
-      readline(input_buf, read_col_from_input_buf);
-      read(read_col_from_input_buf, val_mem_i, good_num);
-      next when not good_num; -- ! Skip invalid lines
-      read(read_col_from_input_buf, val_comma);
-      read(read_col_from_input_buf, val_rw_i, good_num);
-      assert good_num report "Invalid value for rw_i_test";
-      read(read_col_from_input_buf, val_comma);
-      read(read_col_from_input_buf, val_burst_i, good_num);
-      assert good_num report "Invalid value for burst_i_test";
-      read(read_col_from_input_buf, val_comma);
-      read(read_col_from_input_buf, val_oe_o, good_num);
-      assert good_num report "Invalid value for oe_actual";
-      read(read_col_from_input_buf, val_comma);
-      read(read_col_from_input_buf, val_we_o, good_num);
-      assert good_num report "Invalid value for we_actual";
-      read(read_col_from_input_buf, val_comma);
-      read(read_col_from_input_buf, val_we_me_o, good_num);
-      assert good_num report "Invalid value for we_me_actual";
-
-      -- ! Drive signals for DUT.
-      mem_i_test <= val_mem_i;
-      rw_i_test <= val_rw_i;
-      burst_i_test <= val_burst_i;
-      wait for 200 ns;
-
-      -- ! Write results to output file.
-      write(write_col_to_output_buf, mem_i_test);
-      write(write_col_to_output_buf, string'(","));
-      write(write_col_to_output_buf, rw_i_test);
-      write(write_col_to_output_buf, string'(","));
-      write(write_col_to_output_buf, burst_i_test);
-      write(write_col_to_output_buf, string'(","));
-      write(write_col_to_output_buf, val_oe_o);
-      write(write_col_to_output_buf, string'(","));
-      write(write_col_to_output_buf, val_we_o);
-      write(write_col_to_output_buf, string'(","));
-      write(write_col_to_output_buf, val_we_me_o);
-      write(write_col_to_output_buf, string'(","));
-      write(write_col_to_output_buf, oe_o_test);
-      write(write_col_to_output_buf, string'(","));
-      write(write_col_to_output_buf, we_o_test);
-      write(write_col_to_output_buf, string'(","));
-      write(write_col_to_output_buf, we_me_o_test);
-
-      -- ! Check for mismatches and log results.
-      if val_oe_o /= oe_o_test or val_we_o /= we_o_test or val_we_me_o /= we_me_o_test then
-        write(write_col_to_output_buf, string'(",Error"));
-      else
-        write(write_col_to_output_buf, string'(",OK"));
-      end if;
-
-      writeline(output_buf, write_col_to_output_buf);
+      assert (oe_o_test = test_vectors(i).oe_o and we_o_test = test_vectors(i).we_o and we_me_o_test = test_vectors(i).we_me_o)
+      report "Test " & integer'image(i+1) & " failed. Got " & std_logic'image(oe_o_test) & ", " &
+            std_logic'image(we_o_test) & ", " & std_logic'image(we_me_o_test) &
+            "Expected: " & std_logic'image(test_vectors(i).oe_o) & "," & std_logic'image(test_vectors(i).we_o) & ", " &
+            std_logic'image(test_vectors(i).we_me_o) severity error;
     end loop;
-
-    -- ! Close input and output files.
-    file_close(input_buf);
-    file_close(output_buf);
+    report "Simulation finished successfully.";
     wait;
   end process testbench;
 
